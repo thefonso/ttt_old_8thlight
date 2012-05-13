a7d11b1f04efb466bdd296b7a905078e7b3acd94
Alfonso <alephmage@gmail.com>
2012-05-12 20:27:45 -0500

    working on ai

--- player.rb
+++ player.rb
@@ -8,7 +8,6 @@ class Player
   def move_human(game)
     puts "human move..."
     @move = gets.chomp
-    #check thegrid for... ans. exist?
   end
 
   def move_computer(game)
@@ -16,16 +15,6 @@ class Player
     #looks for possible winning move in "win_moves"
     #for now ....acts like human
     @move = gets.chomp
-    #looks in thegrid for answer...if exist...
-    #...try again...else...place answer
-  end
-
-  def win_moves
-
-  end
-
-  def taken
-    taken_array = []
   end
 
 end
--- rubytactoe.rb
+++ rubytactoe.rb
@@ -14,9 +14,28 @@ class Game
         :c1=>" ", :c2=>" ", :c3=>" "
     }
 
-    @taken = []
-  end
+    #note turn this into a method and pass piece as var
+    @win_moves = {
+      :wm01 => {:a1=>"X", :a2=>" ", :a3=>" ", :b1=>" ", :b2=>"X", :b3=>" ", :c1=>" ", :c2=>" ", :c3=>" "},
+      :wm02 => {:a1=>" ", :a2=>"X", :a3=>" ", :b1=>" ", :b2=>"X", :b3=>" ", :c1=>" ", :c2=>" ", :c3=>" "},
+      :wm03 => {:a1=>" ", :a2=>" ", :a3=>"X", :b1=>" ", :b2=>"X", :b3=>" ", :c1=>" ", :c2=>" ", :c3=>" "},
+      :wm04 => {:a1=>" ", :a2=>" ", :a3=>" ", :b1=>"X", :b2=>"X", :b3=>" ", :c1=>" ", :c2=>" ", :c3=>" "},
+      :wm05 => {:a1=>" ", :a2=>" ", :a3=>" ", :b1=>" ", :b2=>"X", :b3=>"X", :c1=>" ", :c2=>" ", :c3=>" "},
+      :wm06 => {:a1=>" ", :a2=>" ", :a3=>" ", :b1=>" ", :b2=>"X", :b3=>" ", :c1=>"X", :c2=>" ", :c3=>" "},
+      :wm07 => {:a1=>" ", :a2=>" ", :a3=>" ", :b1=>" ", :b2=>"X", :b3=>" ", :c1=>" ", :c2=>"X", :c3=>" "},
+      :wm08 => {:a1=>" ", :a2=>" ", :a3=>" ", :b1=>" ", :b2=>"X", :b3=>" ", :c1=>" ", :c2=>" ", :c3=>"X"},
+      #check those corners
+      :wm09 => {:a1=>"X", :a2=>"X", :a3=>" ", :b1=>" ", :b2=>" ", :b3=>" ", :c1=>" ", :c2=>" ", :c3=>" "},
+      :wm10 => {:a1=>"X", :a2=>" ", :a3=>" ", :b1=>"X", :b2=>" ", :b3=>" ", :c1=>" ", :c2=>" ", :c3=>" "},
+      :wm11 => {:a1=>" ", :a2=>"X", :a3=>"X", :b1=>" ", :b2=>" ", :b3=>" ", :c1=>" ", :c2=>" ", :c3=>" "},
+      :wm12 => {:a1=>" ", :a2=>" ", :a3=>"X", :b1=>" ", :b2=>" ", :b3=>"X", :c1=>" ", :c2=>" ", :c3=>" "},
+      :wm13 => {:a1=>" ", :a2=>" ", :a3=>" ", :b1=>" ", :b2=>" ", :b3=>"X", :c1=>" ", :c2=>" ", :c3=>"X"},
+      :wm14 => {:a1=>" ", :a2=>" ", :a3=>" ", :b1=>" ", :b2=>" ", :b3=>" ", :c1=>" ", :c2=>"X", :c3=>"X"},
+      :wm15 => {:a1=>" ", :a2=>" ", :a3=>" ", :b1=>" ", :b2=>" ", :b3=>" ", :c1=>"X", :c2=>"X", :c3=>" "},
+      :wm16 => {:a1=>" ", :a2=>" ", :a3=>" ", :b1=>"X", :b2=>" ", :b3=>" ", :c1=>"X", :c2=>" ", :c3=>" "},
+    }
 
+  end
 
     #display grid on console
     def drawgrid
@@ -38,21 +57,20 @@ class Game
 
       9.times do
         if turn.even?
-          @player = @player_h.move_human("X")
+          @player = @player_h.move_human("O")
           @marker = @player_h.boardpiece
 
           @move = @player
           index = @move.to_sym
           does_move_exist(index,@marker)
         else
-          @player = @player_c.move_computer("O")
+          @player = @player_c.move_computer("X")
           @marker = @player_c.boardpiece
 
           @move = @player
           index = @move.to_sym
           does_move_exist(index,@marker)
         end
-
 
        drawgrid
        turn += 1
@@ -74,10 +92,21 @@ class Game
       
     end
 
-    def win
-      #match current answers located in #thegrid
-      #with possable answers array
-      false
+    def ai_move
+      @anskey={
+        :wm01=>":c3",:wm02=>":c2",:wm03=>":c1",:wm04=>":b3",:wm05=>":b1",:wm06=>":a3",:wm07=>":a2",:wm08=>":a1",
+        :wm09=>":a3",:wm10=>":c1",:wm11=>":a1",:wm12=>":c3",:wm13=>":c3",:wm14=>":c1",:wm15=>":c3",:wm16=>":a1"
+      }
+      #match current answers located in @thegrid with possable answers array iterate for each item
+      @win_moves.each {|key, value| 
+        #check for truth
+        if value==@thegrid 
+          #send key name to var
+          Puts "a winning move was found"
+        else
+          Puts "keep playing"
+        end 
+      }
     end
 end
 
