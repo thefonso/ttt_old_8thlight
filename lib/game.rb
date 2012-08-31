083e7c4f2233ac95c86241f2b2c32769ec058e12
alfonso rush <alephmage@gmail.com>
2012-08-30 20:34:51 -0500

    first test passes

--- lib/game.rb
+++ lib/game.rb
@@ -3,12 +3,40 @@ require_relative "player"
 #Just a Tic Tac Toe game class
 class Game
   #create players
-  initialize
+  def initialize(player_h, player_c)
+    #bring into existence the board and the players
+    @player_h = player_h
+    @player_c = player_c
+    #value hash for the grid lives here
+    $thegrid = {
+        :a1=>" ", :a2=>" ", :a3=>" ",
+        :b1=>" ", :b2=>" ", :b3=>" ",
+        :c1=>" ", :c2=>" ", :c3=>" "
+    }
+    #make a global var for drawgrid used by player
+    $gamegrid = drawgrid
+  
+  end
   #display grid on console
-  drawgrid
+  def drawgrid
+    
+    board = "\n"
+    board << "a #{$thegrid[:a1]}|#{$thegrid[:a2]}|#{$thegrid[:a3]} \n"
+    board << "----------\n"
+    board << "b #{$thegrid[:b1]}|#{$thegrid[:b2]}|#{$thegrid[:b3]} \n"
+    board << "----------\n"
+    board << "c #{$thegrid[:c1]}|#{$thegrid[:c2]}|#{$thegrid[:c3]} \n"
+    board << "----------\n"
+    board << "  1 2 3 \n"
+    return board
+    
+  end
   #start the game
-  play
-  #draw the board
-  #make a move
-  #alternate player turns
+  def play
+    #draw the board
+    puts drawgrid
+    #make a move
+    #alternate player turns
+  end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,8 +3,14 @@ require_relative '../spec_helper'
 # the universe is vast and infinite....and...it is empty
 
 describe "the game" do
-  it "must have a game grid" do
-    pending "initialize game"
+  it "must output a 3x3 game grid on the CLI" do
+    player_h = double('human', :player_h => "X")
+    player_c = double('computer', :player_c => "O")
+    game = Game.new(player_h, player_c)
+    
+    STDOUT.should_receive(:puts).with("\na #{$thegrid[:a1]}|#{$thegrid[:a2]}|#{$thegrid[:a3]} \n----------\nb #{$thegrid[:b1]}|#{$thegrid[:b2]}|#{$thegrid[:b3]} \n----------\nc #{$thegrid[:c1]}|#{$thegrid[:c2]}|#{$thegrid[:c3]} \n----------\n  1 2 3 \n")
+    
+    game.play
   end
   it "must have a human player" do
     pending "human is X"
