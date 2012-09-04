1a5393561a18141143f43a1c1d676448bf7d726a
alfonso rush <alephmage@gmail.com>
2012-09-04 00:54:13 -0500

    test weary

--- lib/player.rb
+++ lib/player.rb
@@ -1,19 +1,19 @@
-# I exist to hold each player and the player's value
-
-class Player
-  def initialize(letter) #Player receives a letter value, We'll use this to identify the play as human or ai
-    @baordpiece = letter
-  end
+    # I exist to hold each player and the player's value
   
-  def move_human(letter)
-    puts "human move"
-    @move = gets.chomp
-    return @move
-  end
+    class Player
+      def initialize(letter) #Player receives a letter value, We'll use this to identify the play as human or ai
+        @baordpiece = letter
+      end
   
-  def move_computer(letter)
-    puts "computer move"
-    # @move = gets.chomp
-    # return @move
-  end
-end
+      def move_human(letter)
+        puts "human move"
+        @move = gets.chomp
+        return @move
+      end
+  
+      def move_computer(letter)
+        puts "computer move"
+        # @move = gets.chomp
+        # return @move
+      end
+    end
