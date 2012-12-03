35d3f7618eaf3ab8a8984ac2e0d25c343423643b
alfonso rush <alephmage@gmail.com>
2012-12-02 20:16:32 -0600

    progress save

--- lib/minimax.rb
+++ lib/minimax.rb
@@ -3,41 +3,25 @@
 module Algorithm
   class Minimax # I return the best_move (win or block)
     include Library # TODO namespace these methods
-
-    def score(board)  # scores move as win or lose
-      if is_a_computer_win(board) == 1
-        return 1
-      elsif is_a_human_win(board) == -1
-        return -1
-      end
-    end
-
-    def best_answers(board)
-      best_answer = 0
-      scores = {}
-      scores = max_move(board).merge(min_move(board))
-      p scores
-      scores.each do |k,v|
-        if v == 1000
-          best_answer = k; break
-        elsif v == -1000
-          best_answer = k; break
-        end
-      end
-      p 'BEST_ANSWER'
-      p best_answer = scores.min.first
-      return best_answer
+    
+    def minmax(board)
+      return max_move(board)
     end
 
     def max_move(board)
       ai_winmove = attempt_win(board)
-
+      #what ply here?      
       scoreboard = {}
       scoreboard[ai_winmove] = 1000
-      scoreboard
+
+      #compare values find greatest value
+      scores = {}
+      scores = scoreboard.merge(min_move(board))
+      
     end
 
     def min_move(board)
+      #what ply here?
       code_plugin = ""
       human_winmove = block_human_win(board,code_plugin)
 
